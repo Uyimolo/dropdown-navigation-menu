fb4e12ea1bc6654a33abdb383e5112a62b87c4f9
Uyimolo <108616062+Uyimolo@users.noreply.github.com>
2022-09-14 20:28:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-document.querySelectorAll(".menu").forEach(menu => {
-    menu.addEventListener("click", () => {
-        document.querySelector(".header-content").classList.toggle("show-nav");
-    });
-    
+header = document.querySelector(".header-content");
+blanket = document.querySelector(".blanket");
+
+document.querySelectorAll(".menu").forEach((menu) => {
+  menu.addEventListener("click", () => {
+    header.classList.toggle("show-nav");
+    blanket.classList.toggle("cover-me");
+  });
 });
 
-document.querySelectorAll(".nav-list").forEach(navList => {
-    navList.addEventListener("click" ,(e) => {
-        const arrow = e.target.querySelector(".arrow");
-        const dropDown = e.target.querySelector(".drop")
-        if(dropDown) {
-       if(!dropDown.classList.contains("show-drop-down")) {
-            dropDown.classList.add("show-drop-down")
-            dropDown.style.maxHeight = dropDown.scrollHeight + "px";
-            arrow.style.transform = "rotateZ(180deg)";
-       }
-       else{
-        dropDown.classList.remove("show-drop-down")
-        dropDown.style.maxHeight = 0;
-        arrow.style.transform = "rotateZ(0)";
-       }
+document.querySelector(".blanket").addEventListener("click", (e) => {
+  header.classList.toggle("show-nav");
+  e.target.classList.remove("cover-me");
+});
+
+document.querySelectorAll(".drop-parent").forEach((navList) => {
+  navList.addEventListener("click", (e) => {
+    const arrow = e.target.querySelector(".arrow");
+    const dropDown = e.target.querySelector(".drop");
+    if (!dropDown.classList.contains("show-drop-down")) {
+      dropDown.classList.add("show-drop-down");
+      dropDown.style.maxHeight = dropDown.scrollHeight + 5 + "px";
+      console.log(dropDown.getBoundingClientRect);
+      arrow.style.transform = "rotateZ(180deg)";
+    } else {
+      dropDown.classList.remove("show-drop-down");
+      dropDown.style.maxHeight = 0;
+      arrow.style.transform = "rotateZ(0)";
     }
-    })
-})
+  });
+});
